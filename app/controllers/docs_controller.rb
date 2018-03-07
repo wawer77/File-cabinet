ab5f1af32846d790c788d309ece5cc8f62be4fc2
wawer77 <tomek.06@wp.pl>
2018-03-07 10:41:34 +0100

    Doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,33 @@
+class DocsController < ApplicationController
+#index for view
+  def index
+  end
+#show for view
+  def show
+  end
+#for view
+  def new
+  end
+#method to create, but does not have view
+  def create
+  end
+
+  def edit
+  end
+#does not have a view
+  def update
+  end
+
+  def destroy
+  end
+
+#prive so they are applied above but are not shown? to reduce redundancy
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+
+end
