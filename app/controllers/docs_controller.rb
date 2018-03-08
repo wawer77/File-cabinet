679ba73dbf51b624904a064272c4a5f3320dd588
wawer77 <tomek.06@wp.pl>
2018-03-08 15:29:49 +0100

    Change index to show current user's docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 #index for view
   def index
-    @docs = Doc.all.order("created_at DESC")
+    @docs = Doc.where(user_id: current_user).order("created_at DESC")
 
   end
 #show for view
